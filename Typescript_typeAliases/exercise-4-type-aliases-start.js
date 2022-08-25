d3433f15a0ef2e9b2eae879e634fd90b309d69cc
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-25 13:09:15 +0200

    exercise: Typescript-typeAliases done

---
+++ Typescript_typeAliases/exercise-4-type-aliases-start.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var country1 = "Nigeria";
+var country2 = "Italy";
+var country3 = "China";
+console.log("The first country is: ".concat(country1, ",\n\nthe second country is: ").concat(country2, ",\n\nthe last country is: ").concat(country3, "."));
---
+++ Typescript_typeAliases/exercise-4-type-aliases-start.ts
@@ -0,0 +1,22 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Create a `Country` type alias for the `string` type.
+// Change the type for each variable to use the `Country` type.
+
+type Country= string;
+
+const country1: Country = "Nigeria";
+
+const country2: Country = "Italy";
+
+const country3: Country = "China";
+
+console.log(`The first country is: ${country1},\n
+the second country is: ${country2},\n
+the last country is: ${country3}.`)
+
+// ----
+
+export {};
