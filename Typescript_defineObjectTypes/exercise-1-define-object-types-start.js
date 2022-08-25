1f8b8625c51ab8ba0757e02fd626d91c7d3d67b2
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-25 13:44:59 +0200

    exercise: Typescript-defineObjectTypes done

---
+++ Typescript_defineObjectTypes/exercise-1-define-object-types-start.js
@@ -0,0 +1,27 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+// Add a type annotation to this variable that describes the object it contains.
+var countryPopulation = {
+    code: "NZ",
+    population: 5135300
+};
+var countryData = {
+    name: "India",
+    code: "IN",
+    population: 1352642280
+};
+var currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€"
+};
+// ----
+console.log("\n");
+console.log("****************** DATA ******************");
+console.log("** Country Population: ".concat(countryPopulation.code, ", ").concat(countryPopulation.population, "      **"));
+console.log("** Country Data: ".concat(countryData.name, ", ").concat(countryData.code, ", ").concat(countryData.population, "  **"));
+console.log("** Currency Data: ".concat(currencyData.name, ", ").concat(currencyData.code, ", ").concat(currencyData.symbol, "          **"));
+console.log("******************************************");
