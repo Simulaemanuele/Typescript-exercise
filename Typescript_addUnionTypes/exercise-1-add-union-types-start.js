40c717a0083a816d4d9108f77190665be05d1bb9
Emanuele Simula <simulaemanuele@gmail.com>
2022-08-26 16:36:59 +0200

    exercise: Typescript-addUnionTypes done

---
+++ Typescript_addUnionTypes/exercise-1-add-union-types-start.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var indianCurrency = "Indian rupee";
+var narniaCurrency = false;
+function outputCountryData(country, data) {
+    console.log("The data for ".concat(country, " is: ").concat(data));
+}
+outputCountryData("Italy", 60317116);
+outputCountryData("Nigeria", "Naira");
+outputCountryData("United States of America", "English");
+outputCountryData("India", 1352642280);
